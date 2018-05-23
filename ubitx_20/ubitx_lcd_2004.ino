b375b7e9e4d247cda300dbaf23fc287c10aa1f38
phdlee <kd8cec@gmail.com>
2018-05-23 15:20:10 +0900

    modified some comments

--- ubitx_20/ubitx_lcd_2004.ino
+++ ubitx_20/ubitx_lcd_2004.ino
@@ -1,10 +1,7 @@
 /*************************************************************************
-  KD8CEC's uBITX Display Routine for LCD2004 Parrel
-  1.This is the display code for the default LCD mounted in uBITX.
-  2.Display related functions of uBITX.  Some functions moved from uBITX_Ui.
-  3.uBITX Idle time Processing
-    Functions that run at times that do not affect TX, CW, and CAT
-    It is called in 1/10 time unit.
+  KD8CEC's uBITX Display Routine for LCD2004 Parrel & I2C
+  1.This is the display code for the 20x04 LCD
+  2.Some functions moved from uBITX_Ui.
 -----------------------------------------------------------------------------
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
